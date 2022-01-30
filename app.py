f2337c1b3a301ed7e942f1bece11b83427910eda
JiyuanLyu <97059580+JiyuanLyu@users.noreply.github.com>
2022-01-30 05:23:28 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+from flask import Flask, g, render_template, request
+import sqlite3
+app = Flask(__name__)
 
+@app.route("/")
+def main():
+    return render_template("base.html")
+
+@app.route('/submit/', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'GET':
+        return render_template('submit.html')
+    else:
+        insert_message(request)
+        return render_template('submit.html', thanks=True)
